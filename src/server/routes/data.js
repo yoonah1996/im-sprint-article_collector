fb683f19666c2128aed28c2a5d49b553ce55aa4c
Lee Hoyong <igotoweb@gmail.com>
2019-03-06 02:59:48 +0900

    change indentation to space

--- src/server/routes/data.js
+++ src/server/routes/data.js
@@ -13,52 +13,52 @@ const { JSDOM } = jsdom;
 
 // GET /data/{lineNo}
 router.get('/:line', async (req, res) => {
-	const filename = `./data/${req.params.line}.txt`;
-	res.set('Content-Type', 'application/json');
+  const filename = `./data/${req.params.line}.txt`;
+  res.set('Content-Type', 'application/json');
 
-	if (fs.existsSync(filename)) {
-		fileHelper
-			.readFile(filename)
-			.then((f) => {
-				res.send(
-					JSON.stringify({
-						id: req.params.line,
-						status: 'exist',
-						body: f
-					})
-				);
-			})
-			.catch((err) => {
-				res.send(
-					JSON.stringify({
-						id: req.params.line,
-						status: 'error',
-						err: err.toString()
-					})
-				);
-			});
-	} else {
-		res.send(
-			JSON.stringify({
-				id: req.params.line,
-				status: 'nonexist'
-			})
-		);
-	}
+  if (fs.existsSync(filename)) {
+    fileHelper
+      .readFile(filename)
+      .then((f) => {
+        res.send(
+          JSON.stringify({
+            id: req.params.line,
+            status: 'exist',
+            body: f
+          })
+        );
+      })
+      .catch((err) => {
+        res.send(
+          JSON.stringify({
+            id: req.params.line,
+            status: 'error',
+            err: err.toString()
+          })
+        );
+      });
+  } else {
+    res.send(
+      JSON.stringify({
+        id: req.params.line,
+        status: 'nonexist'
+      })
+    );
+  }
 });
 
 // POST /data/{lineNo}
 router.post('/:line', async (req, res) => {
-	const lineNo = req.params.line;
-	fileHelper.readLineFromSourceList(lineNo).then((line) => {
-		fetchHelper.retrieveArticle(line).then((html) => {
-			const filename = `./data/${lineNo}.txt`;
-			const dom = new JSDOM(html); // node 환경에서도 DOM element에 접근할 수 있도록 해주는 라이브러리 JSDOM 사용
-			const content = dom.window.document.querySelector('.postArticle-content').textContent;
-			fileHelper.writeFile(filename, content);
-			res.send('ok');
-		});
-	});
+  const lineNo = req.params.line;
+  fileHelper.readLineFromSourceList(lineNo).then((line) => {
+    fetchHelper.retrieveArticle(line).then((html) => {
+      const filename = `./data/${lineNo}.txt`;
+      const dom = new JSDOM(html); // node 환경에서도 DOM element에 접근할 수 있도록 해주는 라이브러리 JSDOM 사용
+      const content = dom.window.document.querySelector('.postArticle-content').textContent;
+      fileHelper.writeFile(filename, content);
+      res.send('ok');
+    });
+  });
 });
 
 module.exports = router;
