02f55db2b988173db3706b1114bf6d0bb3bea9fb
dahaekim88 <dhkim1014@gmail.com>
2019-03-05 13:26:47 +0900

    Add comments for students

--- src/server/index.js
+++ src/server/index.js
@@ -7,8 +7,9 @@ const dataRouter = require('./routes/data');
 const app = express();
 
 app.use(express.static('dist'));
-app.use(bodyParser.text());
+app.use(bodyParser.text()); // http request body를 파싱하는 미들웨어, 미들웨어는 추후 express 스프린트에서 학습 예정
 
+// 라우팅에 대한 간단한 설명은 런코 참조, 자세한 내용은 추후 express 스프린트에서 진행 예정
 app.use('/api/source', sourceRouter);
 app.use('/api/data', dataRouter);
 
