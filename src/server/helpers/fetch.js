8abd70eead5376e34ed0442da73b4e20091f2241
yoonah1996 <yoonah1996@gmail.com>
2020-03-17 14:13:51 +0900

    helper complete

--- src/server/helpers/fetch.js
+++ src/server/helpers/fetch.js
@@ -2,7 +2,28 @@ const https = require('https');
 
 async function retrieveArticle(url) {
   // TODO: retrieve the html string from given url and return as promise
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      // console.log(res)
+      res.on('data', (d) => {
+        // process.stdout.write(d)
+        // console.log(d.toString());
+        resolve(d.toString());
+      })
+      .on('error', (e) => {
+        reject(e);
+      })
+    })
+  })
 }
+
+// https.get('https://medium.com/code-states/good-developer-1-%EC%A2%8B%EC%9D%80-%EA%B0%9C%EB%B0%9C%EC%9E%90%EC%9D%98-5%EA%B0%80%EC%A7%80-%EA%B8%B0%EC%A4%80-b4b9f166caf7', (res) => {
+//   // console.log(res)
+//   res.on('data', (d) => {
+//     // process.stdout.write(d)
+//     console.log(d.toString());
+//   })
+// })
 
 module.exports = {
   retrieveArticle
--- src/server/helpers/file.js
+++ src/server/helpers/file.js
@@ -3,12 +3,26 @@ const fs = require("fs");
 async function writeFile(filename, body) {
   return new Promise((resolve, reject) => {
     // TODO: 특정 파일이름(filename)을 가진 텍스트를 저장할 수 있도록 구현하세요.
+    fs.writeFile(filename, body, (err) => {
+      if(err){
+        reject(err);
+      }else{
+        resolve(body);
+      }
+    })
   });
 }
 
 async function readFile(filename) {
   return new Promise((resolve, reject) => {
     // TODO: 특정 파일이름(filename)을 가진 텍스트를 읽을 수 있도록 구현하세요.
+    fs.readFile(filename, 'utf8' , (err, data) => {
+      if(err){
+        reject(err)
+      }else{
+        resolve(data)
+      }
+    })
   });
 }
 
@@ -22,7 +36,13 @@ async function writeSourceListFile(body) {
 
 async function readLineFromSourceList(nthline) {
   return new Promise((resolve, reject) => {
-    // TODO : ./data/source.txt에 저장되어 있는 텍스트에서 특정 줄에 해당하는 텍스트를 읽을 수 있도록 구현하세요.
+    return fs.readFile('./data/source.txt', 'utf8' , (err, data) => {
+      if(err){
+        reject(err)
+      }else{
+        resolve(data.split('\n')[nthline])
+      }
+    })
   });
 }
 
